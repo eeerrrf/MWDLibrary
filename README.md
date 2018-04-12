3fea17035ae4efd62eb4bf7e498aacd5afc96a20
Waylen Hum <wahum@s.sfusd.edu>
2018-04-12 00:32:58 +0000

    ...

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 <!--# MWDLibrary-->
 <!--Client project-->
 <!--Copy of Client Project Outline for the MWD app  https://docs.google.com/document/d/16TG9dWLA82uAuylmCK_tUFrbQ22bD60i5lPLZ7qWVjk/edit-->
+objective:
+    -have contact info, phone#, and address displyed about the library
+    -be able to search books through: title or author
+            -through Apis
+            -one function for authors (Waylen)
+            -one function through title (deangelo)
+            -disply the information in the website (Marta)
+            
+    -Extras:
+        -Filter by author
+        -submit a review for a book and display it the page
+        -A way to "favorite" a book
--- script.js
+++ script.js
@@ -3,27 +3,41 @@ $(document).ready(function(){
     //clicking button to get input
     $("button").click(function(){
         var input = $("input").val();
-        console.log(input);
         API(input);
     });
     
+   
     //API for a link u guys could change
     function API(searchTerm){
         
         $.ajax({
-            url: "https://www.googleapis.com/books/v1/volumes?q="+searchTerm+"%20potter",
+            url: "https://www.googleapis.com/books/v1/volumes?q=" +searchTerm+ "inauthor:",
             method: "GET",
             success: function(response) {
                 //clear the container so that they can search up the books
-          $(".container").clear();
-                        //displays the book image using the title
-                var url = response.thumbnail[0].images.original.url;
-                $(".container").html ("<img src='+")
+          $("#details").empty();
+                console.log(response);
+                var image = response.items[0].volumeInfo.imageLinks;
+                console.log(image);
+                //appendBookInformationToBody("<img src="+image+">");
+                var author = response.items[0].volumeInfo.author;
+                //appendBookInformationToBody(author);
+                var title = response.items[0].volumeInfo.title;
+                var book = response.items[0].volumeInfo;
+                appendBookInformationToBody(book);
             },
             });
         } 
     
     
+    
+    
+    function appendBookInformationToBody(book) {
+        console.log(book);
+
+    $('body').append(); 
+}
+
     
     
 });
